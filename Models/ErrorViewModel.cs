96a14d2a87eee7c91578231713361591db931ae8
Ramakrishna <ramakrishnamb@gmail.com>
2020-04-30 23:36:06 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppDockerDemoInVS2019.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
